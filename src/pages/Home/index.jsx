d1ff3089d00b7ff2f9c30dcc60f50a2140e5a1b4
higorwilvert <higorwilvert16@gmail.com>
2024-11-03 00:11:52 -0300

    Adicionado a sidebar na home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,20 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import MenuLateral from "../../components/MenuLateral";
+import "../../styles/MenuLateral.css";
+
 function Home() {
+  const location = useLocation();
+  const user = location.state?.user;
+
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <div className="MainLayout">
+      <MenuLateral />
+      <div className="Content">
+        <h1>Bem-vindo, {user ? user.name : "Usuário"}</h1>
+        <p>Aqui está o conteúdo principal do seu aplicativo.</p>
+      </div>
+    </div>
   );
 }
 
