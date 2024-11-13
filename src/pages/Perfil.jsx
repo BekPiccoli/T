1c1f722439f3b2008a1e4faa35841114af5cdc30
Bernardo Piccoli <bernardokpiccoli@gmail.com>
2024-11-13 20:07:25 -0300

    Adiciona função para colocar foto de perfil do usuário

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,22 +1,57 @@
+import { useState } from "react";
 import { useUser } from "../auth/authContext";
-
+import { api } from "../services/api";
 function Perfil() {
+  const [image, setImage] = useState();
   const { user } = useUser();
-  console.log("Obejto user: ", user);
-  // console.log("Acessando obejto user: ", user.name);
-
+  async function photoSubmit(event) {
+    event.preventDefault();
+    try {
+      const formData = new FormData();
+      formData.append("image", image);
+      const headers = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      };
+      await api.post("/upload-image", formData, headers);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+    if (file) {
+      setImage(file);
+      photoSubmit(file);
+    }
+  }
   return (
     <>
       <div className="flex flex-col items-center w-full h-full bg-sky-200">
         <div className="w-11/12 h-72 ml-2 mr-2 mb-2  border-gray-300 bg-[url('./src/assets/planodefundo.jpg')] shadow-xl rounded-b-2xl flex flex-col justify-center items-center">
-          <div className="w-56 h-56 bg-white  border-2 border-black rounded-xl flex justify-center items-center">
-            <img className="mt-4" src=""></img>
-          </div>
+          <form onSubmit={photoSubmit}>
+            <label
+              title="Alterar foto de perfil"
+              className="cursor-pointer w-48 h-44 bg-white border-2 border-black rounded-xl flex justify-center items-center hover:bg-gray-200"
+            >
+              <input
+                type="file"
+                onChange={handleImageChange}
+                className="hidden"
+              />
+              <img
+                className=" w-full h-full rounded-lg"
+                src={image ? URL.createObjectURL(image) : ""}
+                alt="Foto de perfil"
+              />
+            </label>
+          </form>
           <h1 className="text-white">{user ? user.name : "Usuário"}</h1>
         </div>
         <div className="mt-3 w-11/12 h-3/4   border-gray-300 bg-white shadow-xl rounded-t-2xl flex items-center justify-center ">
           <div className=" w-4/5 h-4/5  flex-col flex items-center justify-center">
-            <a className="text-2xl"> Meu perfil</a>
+            <a className="text-2xl mr-10 "> Meu perfil</a>
             <label className="flex flex-col items-center justify-center w-3/6 h-72">
               <div className="flex flex-row items-center w-10/12">
                 <div className="flex flex-col mb-3 w-9/12">
@@ -52,7 +87,7 @@ function Perfil() {
                 <div className="flex flex-col w-9/12">
                   Senha:
                   <input
-                    type="text"
+                    type="password"
                     placeholder="Digite sua senha"
                     id="password"
                     className="px-5 py-3  border border-gray-300 rounded-lg  w-11/12 shadow-sm focus:outline-none focus:border-blue-600 text-lg"
